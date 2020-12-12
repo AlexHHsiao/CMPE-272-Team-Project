c918b06d644c5299bd57a7c052c1fd02d0b19bdd
yohochen <sunny_chen628@hotmail.com>
2020-12-12 14:33:16 -0800

    Display top 5 rating product

--- views/top-five.php
+++ views/top-five.php
@@ -13,17 +13,44 @@ require_once './components/header.php';
 
 <div class="card-container">
     <div class="card">
-        <h5 class="card-header">Top Five</h5>
+        <h5 class="card-header">Top 5 Rating Product &#128293</h5>
         <div class="card-body">
+            <h5 class="list-group-item">
+                Product Name &#128717
+                &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+                <span style="float: right;">Total Rating</span>
+            </h5>
             <div class="list-group">
-                <a href="/product/id" class="list-group-item list-group-item-action">Product Name</a>
-                <a href="/product/product2" class="list-group-item list-group-item-action">Product 1</a>
-                <a href="/product/product3" class="list-group-item list-group-item-action">Product 12</a>
-                <a href="/product/product4" class="list-group-item list-group-item-action">Product 23</a>
-                <a href="/product/product5" class="list-group-item list-group-item-action">Product 36</a>
-                <a href="/product/product5" class="list-group-item list-group-item-action">Product 15</a>
+                <?php
+                    getTop5();
+                ?>
             </div>
         </div>
     </div>
 </div>
 
+<?php
+
+
+function getTop5()
+{
+    $id = $GLOBALS['selectedProduct'];
+    $conn = $GLOBALS['conn'];
+    $sql = "SELECT * FROM (SELECT Id, SUM(Rate) AS totalRate FROM Reviews GROUP BY Id) AS RESULT ORDER BY totalRate DESC LIMIT 5";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $sumRate = $row["totalRate"];
+            $productId = $row["Id"];
+            echo <<<_END
+                <a href="/product/$productId" class="list-group-item list-group-item-action">
+                    Product $productId
+                    &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+                    <span style="float: right; "class="badge badge-primary">$sumRate</span>
+                </a>
+_END;
+        }
+    }
+}
+?>
